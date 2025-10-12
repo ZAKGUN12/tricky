da1ba0f819fd4b3574bb024214a6948c40d6e65d
Zekeriya Akgun <zekeriya.akgun@decathlon.com>
2025-10-12 16:50:52 +0200

    Remove Amplify dependencies - using direct AWS SDK only

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,6 @@
 import type { AppProps } from 'next/app';
-import { Authenticator } from '@aws-amplify/ui-react';
-import { AuthProvider } from '../components/AuthProvider';
-import '../lib/aws-config';
 import '../styles/globals.css';
-import '@aws-amplify/ui-react/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Authenticator.Provider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </Authenticator.Provider>
-  );
+  return <Component {...pageProps} />;
 }
